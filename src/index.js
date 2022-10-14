c3bf116f94d3a28c402ac7a598dcd8cb664d18e1
turbo <joeyturbo@gmail.com>
2022-10-14 09:02:01 -0400

    created Card.js, CardList.js and Robots.js. Built base view for cards

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
-import App from './App';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = createRoot('root');
+// root.render(
+//   <React.StrictMode>
+//     <Hello />
+//   </React.StrictMode>
+// );
+
+const rootElement = 
+document.getElementById('root');
+const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CardList robots={robots}/>
+  </React.StrictMode>,
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
