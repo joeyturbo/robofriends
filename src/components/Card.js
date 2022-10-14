237567e062f3102a45fda32682c762b0d1a3348d
turbo <joeyturbo@gmail.com>
2022-10-14 13:21:07 -0400

    set ErrorBoundary for CardList

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ name, email, id }) => {
     return (
 
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt="robot-photo" src={`https://robohash.org/${id}?200x200`} />
+            <img alt="bot" src={`https://robohash.org/${id}?200x200`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
